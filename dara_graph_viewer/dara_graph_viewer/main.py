02a811465ae7afca9f23ff13c43b9cfdf7ba6e6a
claudiawestby <claudia@causalens.com>
2023-07-26 18:17:50 +0100

    adding connections page for bug

--- dara_graph_viewer/dara_graph_viewer/main.py
+++ dara_graph_viewer/dara_graph_viewer/main.py
@@ -5,6 +5,7 @@ from dara_graph_viewer.pages import (
     IntroductionPage,
     StrongestPathsPage,
 )
+from dara_graph_viewer.pages.connections import ConnectionsPage
 
 # Create a configuration builder
 config = ConfigurationBuilder()
@@ -12,4 +13,5 @@ config = ConfigurationBuilder()
 # Register pages
 config.add_page('Social Networks', IntroductionPage())
 config.add_page('Influential Individuals', InfluentialIndividualsPage())
+config.add_page('Connections', ConnectionsPage())
 config.add_page('Strongest Path', StrongestPathsPage())
