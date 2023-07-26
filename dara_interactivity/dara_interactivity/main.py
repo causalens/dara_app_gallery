d2e441a051d4f10abd8d2af552ca312fbd9f333f
Tamara Stemberga <tamara@192.168.1.126>
2023-07-26 18:17:17 +0100

    add dara logo

--- dara_interactivity/dara_interactivity/main.py
+++ dara_interactivity/dara_interactivity/main.py
@@ -1,4 +1,7 @@
-from dara.core.configuration import ConfigurationBuilder
+from dara.core.configuration import ConfigurationBuilder, Configuration
+from dara.core.visual.components import Menu, RouterContent
+from dara.core.visual.components.sidebar_frame import SideBarFrame
+from dara.core.visual.template import TemplateBuilder
 
 from dara_interactivity.pages.pycomponents_vs_derived_variables import comparison_page
 from dara_interactivity.pages.variables import variables_page
@@ -8,6 +11,24 @@ from dara_interactivity.pages.expensive_calculations import expensive_calculatio
 
 # Create a configuration builder
 config = ConfigurationBuilder()
+
+def template_renderer(config: Configuration):
+    builder = TemplateBuilder(name='side-bar')
+
+    # Using the TemplateBuilder's helper method - add_router_from_pages
+    # to construct a router of page definitions
+    router = builder.add_router_from_pages(list(config.pages.values()))
+
+    builder.layout = SideBarFrame(
+        content=RouterContent(routes=router.content),
+        side_bar=Menu(routes=router.links),
+        logo_path='/static/dara_light.svg',
+    )
+
+    return builder.to_template()
+
+config.add_template_renderer('side-bar', template_renderer)
+config.template = 'side-bar'
 
 # Register pages
 config.add_page('Variables', variables_page())
